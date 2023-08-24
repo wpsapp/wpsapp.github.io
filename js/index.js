ca5c2a52661de468ec76627a66053ba42a50c14a
秋峰 张 <fxzqf@163.com>
2023-08-24 19:30:59 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let cod;
+window.onload = async () => {
+    let urls = new URLSearchParams();
+    let r = urls.get("r");
+    console.log(r);
+};
---
+++ ts/index.ts
@@ -0,0 +1,6 @@
+let cod: string | null;
+window.onload = async () => {
+    let urls = new URLSearchParams();
+    let r= urls.get("r");
+    console.log(r);
+}
