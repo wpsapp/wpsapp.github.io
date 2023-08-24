a3ba469aa61461d141b8d535d2c2f3d1a54a7a0e
秋峰 张 <fxzqf@163.com>
2023-08-24 20:24:25 +0800

    1

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,14 @@
-window.onload=async ()=>{
+//const getCodeUrl="https://developer.kdocs.cn/h5/auth?app_id=AK20220921TSPWLO&scope=user_basic&redirect_uri=" + window.location.href;
+window.onload = async () => {
     let urlsch = new URLSearchParams(location.search)
-    let r=urlsch.get('r');
-    console.log(r);
+    let state = urlsch.get('state');
+    let code = localStorage.getItem('code');
+    let openid = localStorage.getItem('openid');
+    if (state) {
+        if (code && openid) {
+
+        }
+        else
+            window.location.href="https://developer.kdocs.cn/h5/auth?app_id=AK20220921TSPWLO&scope=user_basic&redirect_uri=https://wpsapp.github.io/&state="+state;
+    }
 }
