fb3e5fd7dc9f563d76316f73f459627caf2aa060
秋峰 张 <fxzqf@163.com>
2023-08-25 09:49:35 +0800

    1

--- js/index.js
+++ js/index.js
@@ -9,21 +9,26 @@ window.onload = async () => {
         let http = new XMLHttpRequest();
         http.open("GET", "https://zhibiao.uicp.fun/openid/AK20220921TSPWLO/" + code, false);
         http.send();
-        if (http.readyState == 4) {
+        if (http.readyState == 4)
             openid = http.responseText;
-            http.open("GET", "https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/" + openid + "/" + code, false);
-            http.send();
-            if (http.readyState == 4)
-                token = http.responseText;
-        }
     }
     else {
         code = localStorage.getItem('code');
         openid = localStorage.getItem('openid');
     }
     if (state)
-        if (code && openid)
+        if (code && openid) {
+            let http = new XMLHttpRequest();
+            http.open("GET", "https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/" + openid + "/" + code, false);
+            http.send();
+            if (http.readyState == 4) {
+                token = http.responseText;
+                localStorage.setItem("code", code);
+                localStorage.setItem("openid", openid);
+                localStorage.setItem("token", token);
+            }
             window.location.href = "https://wpsapp.github.io/" + state;
+        }
         else
             window.location.href = "https://developer.kdocs.cn/h5/auth?app_id=AK20220921TSPWLO&scope=user_basic&redirect_uri=https://wpsapp.github.io/&state=" + state;
 };
--- ts/index.ts
+++ ts/index.ts
@@ -4,25 +4,30 @@ window.onload = async () => {
     let state = urlsch.get('state');
     let code = urlsch.get('code');
     let openid: string | null = null;
-    let token: string | null = null;
+    let token: string|null = null;
     if (code) {
         let http = new XMLHttpRequest();
         http.open("GET", "https://zhibiao.uicp.fun/openid/AK20220921TSPWLO/" + code, false);
         http.send();
-        if (http.readyState == 4) {
-            openid = http.responseText;
-            http.open("GET","https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/"+openid+"/" + code,false)
-            http.send();
-            if(http.readyState==4) token=http.responseText;
-        }
+        if (http.readyState == 4) openid = http.responseText;
     }
     else {
         code = localStorage.getItem('code');
         openid = localStorage.getItem('openid');
     }
     if (state)
-        if (code && openid)
+        if (code && openid) {
+            let http = new XMLHttpRequest();
+            http.open("GET", "https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/" + openid + "/" + code, false)
+            http.send();
+            if (http.readyState == 4) {
+                token = http.responseText;
+                localStorage.setItem("code", code);
+                localStorage.setItem("openid", openid);
+                localStorage.setItem("token", token);
+            }
             window.location.href = "https://wpsapp.github.io/" + state;
+        }
         else
             window.location.href = "https://developer.kdocs.cn/h5/auth?app_id=AK20220921TSPWLO&scope=user_basic&redirect_uri=https://wpsapp.github.io/&state=" + state;
 }
