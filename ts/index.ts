da2730230618b6f58f69af16607bb9cd0426c51b
秋峰 张 <fxzqf@163.com>
2023-08-25 09:04:07 +0800

    1

--- ts/index.ts
+++ ts/index.ts
@@ -1,17 +1,36 @@
 window.onload = async () => {
+    
     let urlsch = new URLSearchParams(location.search)
     let state = urlsch.get('state');
-    let openid = localStorage.getItem('openid');
-    let code = localStorage.getItem('code');
-    if (!code)
-        code = urlsch.get('code');
-    if (code) { 
-        
-        if (state) {
+    let code = urlsch.get('code');
+    let openid: string | null;
+    if (code) {
+        let http = new XMLHttpRequest();
+        http.open("Get", "https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/" + code, false);
+        http.send();
+        openid = http.responseText;
+    }
+    else {
+        code = localStorage.getItem('code');
+        openid = localStorage.getItem('openid');
+    }
+    if (code && openid) {
 
-        }
     }
     else
         window.location.href = "https://developer.kdocs.cn/h5/auth?app_id=AK20220921TSPWLO&scope=user_basic&redirect_uri=https://wpsapp.github.io/&state=" + state;
-    
+
+
+
+
+
+
+
+
+}
+function token(openid: string, code: string) {
+    let http = new XMLHttpRequest();
+    http.open("GET", "https://zhibiao.uicp.fun/edittoken/AK20220921TSPWLO/" + openid + "/" + code, false);
+    http.send();
+    return http.responseText;
 }
